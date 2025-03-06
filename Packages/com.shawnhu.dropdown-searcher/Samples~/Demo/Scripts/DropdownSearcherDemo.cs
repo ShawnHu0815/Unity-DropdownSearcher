34e752b2425e6d19f99e5a4c3f0f00da52a61c7e
ShawnHu0815 <979293340@qq.com>
2025-03-06 19:49:00 +0800

    chore: 删除冗余文件和包

--- Packages/com.shawnhu.dropdown-searcher/Samples~/Demo/Scripts/DropdownSearcherDemo.cs
+++ Packages/com.shawnhu.dropdown-searcher/Samples~/Demo/Scripts/DropdownSearcherDemo.cs
@@ -24,4 +24,25 @@ namespace ShawnHu.DropdownSearcher.Samples
         [SearchableEnum]
         public KeyCode SearchableKeyCode;
     }
+
+    public enum TestEnum
+    {
+        [SearchText("第一个选项 First")]
+        First,
+        
+        [SearchText("第二个选项 Second")]
+        Second,
+        
+        [SearchText("第三个选项 Third")]
+        Third,
+        
+        [SearchText("红色 Red")]
+        Red,
+        
+        [SearchText("绿色 Green")]
+        Green,
+        
+        [SearchText("蓝色 Blue")]
+        Blue
+    }
 } 
