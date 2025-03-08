52e1b8ae96f488a63291f19dbd7bcb84acfb314a
ShawnHu0815 <979293340@qq.com>
2025-03-08 18:27:23 +0800

    feat: 优化 Demo 样式

--- Packages/com.shawnhu.dropdown-searcher/Samples~/Demo/Scripts/DropdownSearcherDemo.cs
+++ Packages/com.shawnhu.dropdown-searcher/Samples~/Demo/Scripts/DropdownSearcherDemo.cs
@@ -27,14 +27,14 @@ namespace ShawnHu.DropdownSearcher.Samples
 
     public enum TestEnum
     {
-        [SearchText("第一个选项 First")]
-        First,
+        [SearchText("第一个选项 First Option")]
+        FirstOption,
         
-        [SearchText("第二个选项 Second")]
-        Second,
+        [SearchText("第二个选项 Second Option")]
+        SecondOption,
         
-        [SearchText("第三个选项 Third")]
-        Third,
+        [SearchText("第三个选项 Third Option")]
+        ThirdOption,
         
         [SearchText("红色 Red")]
         Red,
@@ -43,6 +43,56 @@ namespace ShawnHu.DropdownSearcher.Samples
         Green,
         
         [SearchText("蓝色 Blue")]
-        Blue
+        Blue,
+        
+        [SearchText("黄色 Yellow")]
+        Yellow,
+        
+        [SearchText("紫色 Purple")]
+        Purple,
+        
+        [SearchText("橙色 Orange")]
+        Orange,
+        
+        [SearchText("黑色 Black")]
+        Black,
+        
+        [SearchText("白色 White")]
+        White,
+        
+        [SearchText("灰色 Gray")]
+        Gray,
+        
+        [SearchText("粉色 Pink")]
+        Pink,
+        
+        [SearchText("棕色 Brown")]
+        Brown,
+        
+        [SearchText("金色 Gold")]
+        Gold,
+        
+        [SearchText("银色 Silver")]
+        Silver,
+        
+        [SearchText("透明 Transparent")]
+        Transparent,
+        
+        [SearchText("彩虹 Rainbow")]
+        Rainbow,
+        
+        [SearchText("多彩 Multicolor")]
+        Multicolor,
+        
+        [SearchText("荧光 Fluorescent")]
+        Fluorescent,
+        
+        [SearchText("金属 Metallic")]
+        Metallic,
+        
+        [SearchText("哑光 Matte")]
+        Matte
+        
     }
+
 } 
